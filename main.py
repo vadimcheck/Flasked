11e9603adfd5e2f40c4a757ebc38add469bd8cc5
Vadim <vadimibr@gmail.com>
2020-02-18 08:15:06 +0500

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def index():
     return 'Home Page'
@@ -19,4 +20,4 @@ def index():
     return 'Home Page'
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
